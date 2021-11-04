a60db0fd7f10ca9bcc1cc120f19cb4bd16564eb4
yuki <yuki.onishi1129@gmail.com>
2021-11-04 22:09:43 +0900

    add: model

---
+++ my/models.go
@@ -0,0 +1,45 @@
+package my
+
+import "github.com/jinzhu/gorm"
+
+// User model.
+type User struct {
+	gorm.Model
+	Account  string
+	Name     string
+	Password string
+	Message  string
+}
+
+// Post model.
+type Post struct {
+	gorm.Model
+	Address string
+	Message string
+	UserId int
+	GroupId int
+}
+
+// Group model.
+type Group struct {
+	gorm.Model
+	UserId int
+	Name string
+	Message string
+}
+
+// Comment model.
+type Comment struct {
+	gorm.Model
+	UserId int
+	PostId int
+	Message string
+}
+
+// 複数のモデルを一まとめにするモデル
+// CommentJoin join model.
+type CommentJoin struct {
+	Comment
+	User
+	Post
+}
